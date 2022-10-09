85386e6281ae9d59fb68e4e2f0827ef23f02cbec
YrinaSydorko <rozaidoma@gmail.com>
2022-10-09 18:15:02 +0300

    Update task-06.js
    task

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -4,17 +4,18 @@ console.dir(inputName);
 //inputName.classList.remove('valid');
 //inputName.classList.remove('invalid');
 function outOfFocusInputName(event) {
-    inputName.classList.remove('valid');
-    inputName.classList.remove('invalid');
-    console.log(event.currentTarget.value);
-     if (event.currentTarget.value.length === inputName.dataset.length) {
-        //inputName.classList.remove('invalid');
-        inputName.classList.add('valid');
-          } else {
-       // inputName.classList.remove('valid');
-        inputName.classList.add('invalid');
-           }
-    return inputName;  
+  inputName.classList.remove('valid');
+  inputName.classList.remove('invalid');
+  console.log(event.currentTarget.value);
+  if (event.currentTarget.value.length == inputName.dataset.length) {
+    //inputName.classList.remove('invalid');
+    inputName.classList.add('valid');
+  } else {
+    // inputName.classList.remove('valid');
+    inputName.classList.add('invalid');
+  }
+  return inputName;
+}
   //C
-    }
+  
 
