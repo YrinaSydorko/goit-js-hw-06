d9b7bbd96e39f4b27eab29b682c3bc0127f6666f
YrinaSydorko <rozaidoma@gmail.com>
2022-10-09 05:13:13 +0300

    task5

---
+++ src/js/task-05.js
@@ -0,0 +1,8 @@
+const nameInputelem = document.querySelector('#name-input');
+const textOutputelem = document.querySelector('#name-output');
+function changeNameIf(event) {
+   if (event.currentTarget.value !== "") {
+     textOutputelem.textContent = event.currentTarget.value;
+   } else {
+     textOutputelem.textContent = "Anonymous";
+    };
