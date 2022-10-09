f338386a357cb83aa3284f758433c7e9b1431351
YrinaSydorko <rozaidoma@gmail.com>
2022-10-09 07:24:25 +0300

    task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
-
+const formRef = document.querySelector('.login-form');
+formRef.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+    if (ElementInternals.value === '' || password.value || '') {
+        allert('Please, fill in all fields of the form!');
+    } else {
+        const formData = {
+            email: email.value,
+            password: password.value,
+        };
+        console.log(formData);
+    }
+    event.currentTarget.reset();
+  }
