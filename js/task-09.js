34cb5dbe6539e6568e1019b3924823f7fa4c31ef
YrinaSydorko <103926511+YrinaSydorko@users.noreply.github.com>
2022-10-27 19:21:41 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,15 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const bodyEl = document.querySelector("body");
 
-const changeColorEl = document.querySelector(".change-color");
+const refs = {
+  body: document.querySelector('body'),
+  colorName: document.querySelector(".color"),
+  changeColorBtn: document.querySelector(".change-color"),
+};
 
-const colorEl = document.querySelector(".color");
+refs.changeColorBtn.addEventListener("click", onChangeColor);
 
-changeColorEl.addEventListener("click", changeBgColorRandom);
-
-function changeBgColorRandom() {
-  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
-  colorEl.textContent = getRandomHexColor();
+function onChangeColor() {
+  const color = getRandomHexColor();
+  refs.colorName.textContent = `${color}`;
+  refs.body.style.backgroundColor = `${color}`;
 }
